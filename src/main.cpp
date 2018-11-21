7b4a3a5d6ea8a28126665d8fcc096b9d2737a11c
TakumiShinoda <godgodx4@gmail.com>
2018-11-22 00:04:49 +0900

    test tone

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,27 @@
 #define TRY_CONNECT_AP 10
 
 ESP8266WebServer server(80);
-IPAddress ip(192, 168, 3, 2);
+IPAddress ip(192, 168, 3, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 void handle(){
   server.send(200, "text/html", "hoge");
 }
 
+void calling(){
+  server.send(200, "text/html", "success");
+  tone(5, 262, 200);
+  delay(1000);
+}
+
 void setup() {
   uint8_t cnt = 0;
 
   Serial.begin(9600);
+
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(STASSID, STAPASS);
+
   WiFi.softAPdisconnect(true);
   WiFi.softAPConfig(ip, ip, subnet);
   WiFi.softAP(APSSID, APPASS);
@@ -45,6 +53,7 @@ void setup() {
   Serial.println(WiFi.softAPIP());
 
   server.on("/", handle);
+  server.on("/call", calling);
   server.begin();
 }
 
